49da378fbbc303c34cfa9a83057ae77e2f0ec137
kmj0792 <kmj0792@naver.com>
2021-07-22 14:54:24 +0900

    그래프 새로고침 완료

--- graph.js
+++ graph.js
@@ -1,70 +1,25 @@
 //좌표값 배열 선언
-/*
-var chartLabels = [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,
-    0.75,0.8,0.85,0.9,0.95,1,1.05,1.1,1.15,1.2,1.25,1.3,1.35,1.4,1.45,1.5,1.55,1.6,
-    1.65,1.7,1.75,1.8,1.85,1.9,1.95,2,2.05,2.1,2.15,2.2,2.25,2.3,2.35,2.4,2.45,2.5,
-    2.55,2.6,2.65,2.7,2.75,2.8,2.85,2.9,2.95,3,3.05,3.1,3.15,3.2,3.25,3.3,3.35,3.4,
-    3.45,3.5,3.55,3.6,3.65,3.7,3.75,3.8,3.85,3.9,3.95,4,4.05,4.1,4.15,4.2,4.25,4.3,
-    4.35,4.4,4.45,4.5,4.55,4.6,4.65,4.7,4.75,4.8,4.85,4.9,4.95,5,5.05,5.1,5.15,5.2,
-    5.25,5.3,5.35,5.4,5.45,5.5,5.55,5.6,5.65,5.7,5.75,5.8,5.85,5.9,5.95,6,6.05,6.1,
-    6.15,6.2,6.25,6.3,6.35,6.4,6.45,6.5,6.55,6.6,6.65,6.7,6.75,6.8,6.85,6.9,6.95,7,
-    7.05,7.1,7.15,7.2,7.25,7.3,7.35,7.4,7.45,7.5,7.55,7.6,7.65,7.7,7.75,7.8,7.85,7.9,
-    7.95,8,8.05,8.1,8.15,8.2,8.25,8.3,8.35,8.4,8.45,8.5,8.55,8.6,8.65,8.7,8.75,8.8,8.85,
-    8.9,8.95,9,9.05,9.1,9.15,9.2,9.25,9.3,9.35,9.4,9.45,9.5,9.55,9.6,9.65,9.7,9.75,9.8,
-    9.85,9.9,9.95,10,10.05,10.1,10.15,10.2,10.25,10.3,10.35,10.4,10.45,10.5,10.55,10.6,
-    10.65,10.7,10.75,10.8,10.85,10.9,10.95,11,11.05,11.1,11.15,11.2,11.25,11.3,11.35,
-    11.4,11.45,11.5,11.55,11.6,11.65,11.7,11.75,11.8,11.85,11.9,11.95,12,12.05,12.1,
-    12.15,12.2,12.25,12.3,12.35,12.4,12.45,12.5,12.55,12.6,12.65,12.7,12.75,12.8,12.85,
-    12.9,12.95,13,13.05,13.1,13.15,13.2,13.25,13.3,13.35,13.4,13.45,13.5,13.55,13.6,13.65,
-    13.7,13.75,13.8,13.85,13.9,13.95,14,14.05,14.1,14.15,14.2,14.25,14.3,14.35,14.4,14.45,
-    14.5,14.55,14.6,14.65,14.7,14.75,14.8,14.85,14.9,14.95,15,15.05,15.1,15.15,15.2,15.25,
-    15.3,15.35,15.4,15.45,15.5,15.55,15.6,15.65,15.7,15.75,15.8,15.85,15.9,15.95,16,16.05,
-    16.1,16.15,16.2, 16.25,16.3,16.35,16.4,16.45,16.5,16.55,16.6,16.65,16.7,16.75,16.8,16.85,
-    16.9,16.95,17,17.05,17.1,17.15,17.2,17.25,17.3,17.35,17.4,17.45,17.5
-    ]; //예시 x좌표값
-var chartData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.001,
-    0, -0.001, 0, 0, 0, 0, 0, 0.001, 0, -0.001, 0.001, 0.001, -0.001, 0, 0.001, -0.001, 0, 0, -0.001, 
-    0.001, 0.001, -0.001,0, 0, -0.001, 0.001, 0, -0.001, 0.001, 0, -0.001, 0.001, 0, -0.001, 0.001, 0, 
-    -0.001, 0.001, 0, 0, 0.001, -0.001, -0.001, 0.001, 0, 0, 0.001, -0.001, 0, 0.001, 
-    -0.001, 0, 0.001, -0.001, 0, 0.001, -0.001, 0, 0.001, -0.001, 0, 0.001, -0.001, 0, 0.001, -0.001, 
-    0, 0.001, -0.001, 0, 0.001, -0.001, 0, 0.001, -0.001, 0, 0.001, -0.00, 0, 0.001, 0, 0, 0, -0.00, 
-    0, 0.001, -0.00, 0, 0.001, 0, 0, -0.001, 0, 0.001, -0.001, -0.001, 0.001, 0, 0, 0.001, 
-    -0.001, -0.001, 0.001, 0, -0.001, 0.001, 0, 0, 0.001, -0.001, -0.001, 0.001, 0.001, 0, 0, -0.001, 
-    -0.001, 0.001, 0.001, 0, 0, -0.001, 0, 0.001, 0, 0, 0, -0.001, 0, 0.001, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    -0.001, -0.001, 0.001, 0.001, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0.002, 0.349, 0.959, 1.216, 1.209, 
-    1.204, 1.197, 0.781, -0.314, -0.975, -0.943, -0.907, -0.868, -0.842, -0.812, -0.654, -0.273, -0.047, 
-    -0.067, -0.055, -0.03, -0.016, -0.004, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0
-    ]; //예시 y좌표값
-*/
-var chartLabels =[0,1,2,3,4,5,6,7,8,9];
-var chartData =[1,1,2,3,3,3,5,4,7,10];
+var chartLabels =[1,2,3,4,5,6,7,8,9,10];
+var chartData =[1,1,1,2,2,3.5,4,7,6,5];
+
 var xRange=[]; //드래그된 x범위
 var yRange=[]; //드래그된 y범위
+
 var max_value; //피팅할 부분의 최대 인덱스값
 var min_value; //피팅할 부분의 최소 인덱스값
+
 var chartLabels_zoom=[]; //피팅할 범위 x
 var chartData_zoom = []; //피팅할 범위 y
-var a; // 함수의 계수 a
-var b; // 함수의 계수 b
-var c; // 함수의 계수 c
-var a10; // 함수의 계수 a의 반올림한 값
-var b10; // 함수의 계수 b의 반올림한 값
-var c10; // 함수의 계수 c의 반올림한 값
 
-var arr;//x좌표를 행렬로 받아올 변수 
-var t_arr;//전치행렬
+var a,b,c; // 함수의 계수 a,b,c
+var a10,b10,c10; // 함수의 계수 반올림한 값
+
+var arr,t_arr,inverted,abc;//x좌표의 행렬, 전치행렬, 역행렬, 계수행렬
 var arr_y=new Array(1); //y좌표를 행렬로 받아올 변수 
+var memo;
 
-var temp=[]; //좌표 제거할때 사용 
-var inverted;//역행렬
 var Coe;//(A_t A)'(A_t)까지 한 것
-var abc; //이차 방정식 계수 행렬 abc[0]=c
-var type=0; //옵션 선택 값 받아옴
+var type=0; //옵션 선택 값
 
 // 피팅함수 종류가 뭔지 type을 숫자로 받아오는 함수
 function change_Type(e){
@@ -74,15 +29,15 @@ function change_Type(e){
 
 
 //다중 그래프 그리기 
-function getGraph(data_x, data_y, fit_x, fit_y, where , mode1 ,mode2 ,color1,color2, type){
-    var Graph=document.getElementById(where);
+function getGraph(data_x, data_y, fit_x, fit_y , type){
+    var Graph=document.getElementById('myGraph');
     var trace1 = {
             x: data_x,
             y: data_y,
             type: 'scatter',
-            mode: mode1, // 점 안찍히고 라인만 
+            mode: 'lines+markers', // 점 안찍히고 라인만 
             line:{
-                color:color1
+                color:"black"
             }
     };
 
@@ -90,9 +45,9 @@ function getGraph(data_x, data_y, fit_x, fit_y, where , mode1 ,mode2 ,color1,col
             x: fit_x,
             y: fit_y,
             type: 'scatter',
-            mode: mode2, // 점 안찍히고 라인만 
+            mode: 'lines', // 점 안찍히고 라인만 
             line:{
-                color:color2
+                color:"red"
             }
     };
     var data = [trace1, trace2];
@@ -109,28 +64,30 @@ function getGraph(data_x, data_y, fit_x, fit_y, where , mode1 ,mode2 ,color1,col
     };
     Plotly.newPlot(Graph, data, layout,config);
     if (type==1){
-        
-        Plotly.relayout(where, 'title',`y = ${a10}x + ${b10}`);
+        document.getElementById('fitting_result').innerHTML="f(X) = " + a10 +"x + (" +b10+")"
+        //Plotly.relayout(where, 'title',`y = ${a10}x + ${b10}`);
     }else if (type==2){
-     
-        Plotly.relayout(where, 'title',`y = ${a10}x² + ${b10}x + ${c10}`);
+        document.getElementById('fitting_result').innerHTML="f(X) = " + a10 +"x² + (" +b10+")x + (" + c10+")"
+        //Plotly.relayout(where, 'title',`y = ${a10}x² + ${b10}x + ${c10}`);
     }else if(type==3){
-        Plotly.relayout(where, 'title',`y = ${a10} sin(x) + ${b10}`);
+        document.getElementById('fitting_result').innerHTML="f(X) = " + a10 +"sin(x) + (" +b10+")"
+        //Plotly.relayout(where, 'title',`y = ${a10} sin(x) + ${b10}`);
     }else if(type==4){
-        Plotly.relayout(where, 'title',`y = ${a10} cos(x) + ${b10}`);
+        document.getElementById('fitting_result').innerHTML="f(X) = " + a10 +"cos(x) + (" +b10+")"
+        //Plotly.relayout(where, 'title',`y = ${a10} cos(x) + ${b10}`);
     }
 }
-
-//영역 선택할 수 있는 그래프 그리기
-function getGraph_select_range(data_x, data_y, where , mode , color){
-    var Graph = document.getElementById(where);
+//document.getElementById('range').innerHTML="x range : "+ "["+ xRange.map(formatter).join(' ~ ')+"]"+ "<br>"+"y range : "+ "["+yRange.map(formatter).join(' ~ ')+"]"
+//영역 선택할 수 있는 그래프 
+function getGraph_select_range(data_x, data_y){
+    var Graph = document.getElementById('myGraph');
     var d3 = Plotly.d3;
     var formatter = d3.format('.3f');
     var data=[{
-        mode: mode,
+        mode: 'lines+markers',
         x: data_x,
         y: data_y,
-        line:{color:color}
+        line:{color:'black'}
     }];
     var layout={
         title : 'graph',
@@ -143,16 +100,17 @@ function getGraph_select_range(data_x, data_y, where , mode , color){
         modeBarButtonsToRemove: ['lasso2d','autoScale2d','toggleSpikelines'], //plotly 기본 버튼 중 제거할 것
         displaylogo:false
     };
-    Plotly.plot(Graph, data, layout, config );
+    Plotly.newPlot(Graph, data, layout, config );
 //Lasso Select , Produced with Plotly
     Graph.on('plotly_selected', (eventData) => {
         xRange = eventData.range.x;
         yRange = eventData.range.y;
-        //document.getElementById('range').innerHTML="x range:"+xRange.map(formatter).join(' , ') + "y range :"+ yRange.map(formatter).join(' , ')
-        Plotly.relayout(where, 'title',
-            `x range: [${xRange.map(formatter).join(' , ')}]<br>y range: [${yRange.map(formatter).join(' , ')}]`
+        document.getElementById('range').innerHTML="x range : "+ "["+ xRange.map(formatter).join(' ~ ')+"]"+ "<br>"+"y range : "+ "["+yRange.map(formatter).join(' ~ ')+"]"
+        Plotly.relayout('myGraph', 'title',
+            `x range : [${xRange.map(formatter).join(' ~ ')}]<br>y range: [${yRange.map(formatter).join(' , ')}]`
         );
-    });          
+    });         
+  
 }
 
 //행렬만들기 - 행렬 반환
@@ -466,7 +424,7 @@ function fit(fit_color, range_color){ //select color
         alert('옵션을 선택해 주세요');
        }
 
-        getGraph(chartLabels_zoom,chartData_zoom,fit_xy[0], fit_xy[1],'myfitting','lines+markers', 'lines' , range_color,fit_color, type); 
+        getGraph(chartLabels_zoom,chartData_zoom,fit_xy[0], fit_xy[1],  type); 
 }
 
 /*
