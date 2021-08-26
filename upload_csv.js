d40171bb60466834b311fa5b161b536522fc81ff
kmj0792 <62427558+kmj0792@users.noreply.github.com>
2021-08-26 09:22:49 +0900

    cos함수 제외

--- upload_csv.js
+++ upload_csv.js
@@ -1,14 +1,18 @@
+//파일업로드에 필요한 코드들 
 const input = document.querySelector('#file');
 
 
 input.addEventListener('change', readExcel);//파일이 바뀌면
 input.addEventListener('click', reset_rows);//파일이 클릭되면
 
+//파일선택 버튼이 클릭될때마다 배열들 초기화하기
 function reset_rows(){
     arrObj.rows=[];
     arrObj.yData=[];
     arrObj.xData=[];
 }
+
+//파일이 바뀔때마다 엑셀을 json으로 
 function readExcel() {
     
     let reader = new FileReader();
